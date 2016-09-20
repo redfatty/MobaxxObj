13b7ad90cede56536e42363449304d11bd050edf
黄炯Tower <huangJiong@huangJiongs-Mac-mini.local>
2016-09-20 17:45:53 +0800

    工程创建

---
+++ Code/Base/Base/Base.h
@@ -0,0 +1,19 @@
+//
+//  Base.h
+//  Base
+//
+//  Created by huangJiong on 16/9/20.
+//  Copyright © 2016年 miwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base.
+FOUNDATION_EXPORT double BaseVersionNumber;
+
+//! Project version string for Base.
+FOUNDATION_EXPORT const unsigned char BaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base/PublicHeader.h>
+
+
---
+++ Code/Network/Network/Network.h
@@ -0,0 +1,19 @@
+//
+//  Network.h
+//  Network
+//
+//  Created by huangJiong on 16/9/20.
+//  Copyright © 2016年 miwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Network.
+FOUNDATION_EXPORT double NetworkVersionNumber;
+
+//! Project version string for Network.
+FOUNDATION_EXPORT const unsigned char NetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Network/PublicHeader.h>
+
+
